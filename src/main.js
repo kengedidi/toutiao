cda893858222c7f19cbbce32f9219f197c21f54e
ken <ken@qq.com>
2021-01-22 15:51:12 +0800

    15-完成个人信息编辑静态页面的制作

--- src/main.js
+++ src/main.js
@@ -9,9 +9,9 @@ import '@/styles/reset.less'
 import router from '@/router/index.js'
 
 //引入vant，调用里面其中的一个方法toast
-import { Toast } from 'vant'
+import { Toast, Uploader } from 'vant'
 // use之后就会在全局的vue实例上挂载一个$toast
-Vue.use(Toast)
+Vue.use(Toast).use(Uploader)
 // 以后每一个组件可以直接使用
 
 new Vue({
--- src/views/editUserInfo.vue
+++ src/views/editUserInfo.vue
@@ -1,25 +1,71 @@
 <template>
   <div class="editUserInfo">
-    <!-- 顶部 -->
+    <!-- 顶部区域 -->
     <myheader title="编辑资料">
-       <!-- slot:指定内容放置到那个插槽中 -->
+      <!-- slot:指定内容放置到那个插槽中 -->
       <span class="iconfont iconjiantou2" slot="left"></span>
-      <span class="van-icon van-icon-wap-home-o" slot="right"></span>
+      <span
+        class="van-icon van-icon-wap-home-o"
+        style="font-size: 22px; color: red"
+        slot="right"
+      ></span>
     </myheader>
-    
+    <!-- 头像区域 -->
+    <div class="userimg">
+      <img class="img" src="../assets/aa.png" alt="加载失败" />
+      <!-- 文件上传组件 -->
+      <van-uploader :after-read="afterRead" />
+    </div>
+    <!-- 单元格区域 -->
+    <hmcell title="昵称" desc="火星人"> </hmcell>
+    <hmcell title="密码" desc="******"> </hmcell>
+    <hmcell title="性别" desc="男"> </hmcell>
   </div>
 </template>
 
 <script>
 // 引入封装好的头部组件
-import myheader from '@/components/myheader.vue'
+import myheader from "@/components/myheader.vue";
+// 引入封装好的单元格
+import hmcell from "@/components/hmcell.vue";
 export default {
-    components: {
-      myheader
-    }
-}
+  components: {
+    myheader,
+    hmcell,
+  },
+  methods: {
+    afterRead(file) {
+      // 此时可以自行将文件上传至服务器
+      console.log(file);
+    },
+  },
+};
 </script>
 
 <style lang="less" scoped>
+.userimg {
+  padding: 30px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  .img {
+    width: 100 * 100vw / 360;
+    height: 100 * 100vw / 360;
+    border-radius: 50%;
+  }
 
+  /deep/ .van-uploader {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    opacity: 0;
+  }
+  /deep/ .van-uploader__upload {
+    width: 100 / 360 * 100vw;
+    height: 100 / 360 * 100vw;
+    margin: 0;
+  }
+}
 </style>
