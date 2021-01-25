938a42a1c12e232a13c37189a32f06ebfedae21b
ken <ken@qq.com>
2021-01-25 16:08:50 +0800

    28-加载不同栏目的新闻数据

--- src/views/index.vue
+++ src/views/index.vue
@@ -23,9 +23,14 @@
     <!-- active就代表当前默认选中的标签项：那么我们就应该加载这个选项的新闻数据 -->
     <van-tabs swipeable v-model="active">
       <!-- 生成栏目列表 --- 遍历栏目所有数据 -遍历数组  -->
-      <van-tab v-for="(value, index) in catelist" :title="value.name">
+      <!-- :key="value.id+:key="item.id" 唯一标示 加了vscode编辑器就不会有波浪线报错，对业务没有作用 -->
+      <van-tab v-for="value in catelist" :title="value.name" :key="value.id">
         <!-- 生成这个栏目的新闻列表 -->
-          <articleBlock v-for="item in value.postlist" :post='item'></articleBlock>
+        <articleBlock
+          v-for="item in value.postlist"
+          :post="item"
+          :key="item.id"
+        ></articleBlock>
       </van-tab>
     </van-tabs>
   </div>
@@ -35,10 +40,10 @@
 import { getCateList } from "@/apis/category.js";
 import { getPostList } from "@/apis/post.js";
 // 引入封装好的文章块
-import articleBlock from '@/components/articleBlock.vue'
+import articleBlock from "@/components/articleBlock.vue";
 export default {
   components: {
-    articleBlock
+    articleBlock,
   },
   data() {
     return {
@@ -63,19 +68,12 @@ export default {
     this.catelist = this.catelist.map((value) => {
       return {
         ...value,
-        postlist:[]
+        postlist: [],// 添加了存储这个栏目新闻数据的数组
       };
     });
 
-    //--------------------------
-    // 根据栏目ID获取对应栏目的文章数据
-    let list = await getPostList(this.catelist[this.active].id);
-    // console.log(list);
-    // 实现栏目数据的动态渲染
-    // this.postlist = list.data.data;  //原来未对数据进行改造的
-    // 将获取数据存储到当前栏目的postlist数组中
-    this.catelist[this.active].postlist = list.data.data
-    console.log(this.catelist);
+    //页面一打开就加载当前栏目对应文章数据（栏目已经设置如果登陆过就下标1，没登陆下标就0 ，页面一开始显示头条栏目的文章）
+    this.init();
   },
   watch: {
     // 监听栏目
@@ -85,6 +83,24 @@ export default {
       //  console.log(this.catelist[this.active]); // 索引对应的栏目数据
       //  console.log(this.catelist[this.active].id); //栏目的id
       let id = this.catelist[this.active].id; //栏目的id
+      // 监听用户切换栏目的时候，触发。然后判断postlist数组的长度是否为0，如果为0，也就是之前没有加载过，则发送后台请求 加载当前栏目的数据。 如果postlist数组的长度不是为0，也就是之前加载过了，就未必要再重新加载了，这样可以避免切换栏目每次都是第一页。
+      if(this.catelist[this.active].postlist.length == 0){
+        this.init();
+      }
+    },
+  },
+  methods: {
+    // 封装函数方法（就避免代码重复写）
+    async init() {
+      //--------------------------
+      // 根据栏目ID获取对应栏目的文章数据
+      let list = await getPostList(this.catelist[this.active].id);
+      // console.log(list);
+      // 实现栏目数据的动态渲染
+      // this.postlist = list.data.data;  //原来未对数据进行改造的
+      // 将获取数据存储到当前栏目的postlist数组中
+      this.catelist[this.active].postlist = list.data.data;
+      console.log(this.catelist);
     },
   },
 };
